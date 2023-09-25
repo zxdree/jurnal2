a128e0a8deeb80528236e7d5e6054494f6366091
adre putra <adreputra61@gmail.com>
2023-09-25 15:14:38 +0700

    first commit

---
+++ jurnal1.java
@@ -0,0 +1,8 @@
+public class jurnal1 { 
+    
+	public static void main(String[] args) {
+	    System.out.println("Nama : I Made Adre Udayana Putra");
+        System.out.println("Nim : 607062300080");
+        System.out.println("kelas : D3IF-47-03");
+	}	
+}	
